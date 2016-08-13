a00a3a50212102917e22e976f183ffbf92e0fc68
Brenda Wallace <brenda@brenda.nz>
2016-08-13 16:42:55 +1200

    slightly more helpful error when no args

--- switch.py
+++ switch.py
@@ -2,8 +2,13 @@
 from orvibo.s20 import S20
 import sys
 
-ip = sys.argv[1]
-action = sys.argv[2]
+try:
+  ip = sys.argv[1]
+  action = sys.argv[2]
+except IndexError:
+  print ("IP and action required. e.g. switch.py 10.1.1.1. ON")
+  sys.exit(1)
+  
 
 if(action == "ON"):
   state = True
